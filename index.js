b17c02d80213bee9cb875c237542016c80fee1f7
lucianmarquess <lm8500216@gmail>
2024-08-11 21:19:17 +0100

    Desafio proposto pelo professor Felipe

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("olá")
+let Heroi = "Itachi"
+let nivelDoHeroi = 10001
+
+if (nivelDoHeroi <1000){
+    console.log("O Herói de nome " + Heroi + " está no nivel de Ferro") 
+}else if (nivelDoHeroi >=1001 && nivelDoHeroi <=2000){
+    console.log("O Herói de nome " + Heroi + " está no nivel de Bronze")
+
+}else if (nivelDoHeroi >=2001 && nivelDoHeroi <=5000){
+    console.log("O Herói de nome " + Heroi + " está no nivel de Prata")
+}else if (nivelDoHeroi >=5001 && nivelDoHeroi <=7000){
+    console.log("O Herói de nome " + Heroi + " está no nivel de Ouro")
+}else if (nivelDoHeroi >=7001 && nivelDoHeroi <=8000){
+    console.log("O Herói de nome " + Heroi + "está no nivel de Platina")
+}else if (nivelDoHeroi >=8001 && nivelDoHeroi <=9000){
+    console.log("O Herói de nome " + Heroi + " está no nivel de Ascendente")
+}else if (nivelDoHeroi >=9001 && nivelDoHeroi <=10000){
+    console.log("O Herói de nome " + Heroi + " está nivel de Imortal")
+}else{
+    console.log("O Herói de nome " + Heroi + " está no nivel Radiante")
+} 
+
+
+
+
+
+
